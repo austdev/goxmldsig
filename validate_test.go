878c5f61bef5588aeb137d39a0afefc9193cca52
Serge Lysenko <sergii.lysenko@avid.com>
2022-10-11 21:47:19 +0300

    Remove path map manipulations into types/signature.go & fix ValidateEx()

--- validate_test.go
+++ validate_test.go
@@ -6,8 +6,6 @@ import (
 	"encoding/pem"
 	"testing"
 
-	"github.com/austdev/goxmldsig/etreeutils"
-
 	"github.com/beevik/etree"
 	"github.com/stretchr/testify/require"
 )
@@ -285,24 +283,6 @@ func TestValidateWithModifiedAndSignatureEdited(t *testing.T) {
 
 	_, err = vc.Validate(doc.Root())
 	require.Error(t, err)
-}
-
-func TestMapPathAndRemove(t *testing.T) {
-	doc := etree.NewDocument()
-	err := doc.ReadFromString(`<X><Y/><Y><RemoveMe xmlns="x"/></Y></X>`)
-	require.NoError(t, err)
-
-	el, err := etreeutils.NSFindOne(doc.Root(), "x", "RemoveMe")
-	require.NoError(t, err)
-	require.NotNil(t, el)
-
-	path := mapPathToElement(doc.Root(), el)
-	removed := removeElementAtPath(doc.Root(), path)
-	require.True(t, removed)
-
-	el, err = etreeutils.NSFindOne(doc.Root(), "x", "RemoveMe")
-	require.NoError(t, err)
-	require.Nil(t, el)
 }
 
 const (
